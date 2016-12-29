c704a92809d1872cd192d8d9e9a662ff6a6275b5
Ali Hakak <ali.hakak@gmail.com>
2016-12-28 19:45:37 -0500

    css changes , added submit button

--- react-forum/src/components/PostModule/PostModule.js
+++ react-forum/src/components/PostModule/PostModule.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PostUserName from './PostUserName'
 import PostMessage from './PostMessage'
+import PostSubmit from './PostSubmit'
 import './PostModule.css'
 class PostModule extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class PostModule extends React.Component {
         <div>
           <PostUserName/>
           <PostMessage/>
+          <PostSubmit/>
         </div>
       </form>
       </div>
